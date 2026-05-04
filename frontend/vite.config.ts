9ee4faf7b2af206a25f269699f531810bfe42e6c
JuanMedina05 <juanperezmedina2005@gmail.com>
2026-05-04 14:07:04 +0200

    Configurar vite para desplegar en railway

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,14 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+    allowedHosts: true
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+    allowedHosts: true
+  }
 })
